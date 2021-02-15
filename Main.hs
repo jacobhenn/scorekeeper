cab8a983271475556f7996b3543dbcaa46fd5047
Jacob Henn <windowWeasel@outlook.com>
2021-02-15 13:59:58 -0800

    change table formatting and add basic prefix matching

--- Main.hs
+++ Main.hs
@@ -11,56 +11,53 @@ import Control.Monad (liftM2)
 import Data.Either (fromRight)
 import Text.Printf (printf)
 import Data.Maybe (maybe)
-import System.IO (hFlush, stdout)
-import Data.List (sort)
+import System.IO (hFlush, stdout, writeFile, readFile)
+import Data.List (sort, isPrefixOf)
 import Safe (readMay, headMay, atMay, foldl1May, maximumDef)
 
 --------------------------------------------------------------------------------
-data Player = Player{score :: Int, name :: String} deriving Eq
+data Player = Player{score :: Int, name :: String} deriving (Eq, Show, Read)
 
 -- order players by their scores
 instance Ord Player where
     (Player s _) `compare` (Player s' _) = s `compare` s'
 
-instance Show Player where
-    show (Player s n) = show s ++ " " ++ n
-
 --------------------------------------------------------------------------------
 printHelp :: IO ()
-printHelp = putStrLn " ├───────────────────────────────────────────────────────────────────╮\n\
-                     \ │ scorekeeper v3.3.0 (https://github.com/jacobhenn/scorekeeper)     │\n\
-                     \ ├───────────────────┬───────────────────────────────────────────────┤\n\
-                     \ │ [player]          │ add 1 point to [player]                       │\n\
-                     \ │ [n] [player]      │ add [n] (integer) points to [player]          │\n\
-                     \ │ :add [player] ... │ add [player](s) to the list of players        │\n\
-                     \ │ :rm [player] ...  │ remove [player](s) from the list of players   │\n\
-                     \ │ :mul [n]          │ multiply each player's score by [n] (integer) │\n\
-                     \ │ :q, :quit, :exit  │ exit scorekeeper                              │\n\
-                     \ │ :h, :help         │ show this message                             │\n\
-                     \ ├───────────────────┴───────────────────────────────────────────────╯"
+printHelp = putStrLn "╭┴──────────────────────────────────────────────────────────────────╮\n\
+                     \│ scorekeeper v3.4.0 (https://github.com/jacobhenn/scorekeeper)     │\n\
+                     \├───────────────────┬───────────────────────────────────────────────┤\n\
+                     \│ [player]          │ add 1 point to [player]                       │\n\
+                     \│ [n] [player]      │ add [n] (integer) points to [player]          │\n\
+                     \│ :add [player] ... │ add [player](s) to the list of players        │\n\
+                     \│ :rm [player] ...  │ remove [player](s) from the list of players   │\n\
+                     \│ :mul [n]          │ multiply each player's score by [n] (integer) │\n\
+                     \│ :q, :quit, :exit  │ exit scorekeeper                              │\n\
+                     \│ :h, :help         │ show this message                             │\n\
+                     \╰┬──────────────────┴───────────────────────────────────────────────╯"
 
 --------------------------------------------------------------------------------
 printPlayers :: [Player] -> IO ()
 printPlayers players = do
-    putStrLn $ " ├" ++ replicate (maxNameLength + 2) '─'
-                   ++ "┬"
-                   ++ replicate (maxScoreLength + 2) '─'
-                   ++ "╮"
+    putStrLn $ "╭┴" ++ replicate maxNameLength '─'
+                   ++ "─┬─"
+                   ++ replicate maxScoreLength '─'
+                   ++ "─╮"
 
-    mapM_ (\(Player s n) -> printf " │ %-*s │ %*d │\n" maxNameLength n maxScoreLength s)
+    mapM_ (\(Player s n) -> printf "│ %-*s │ %*d │\n" maxNameLength n maxScoreLength s)
         players
 
-    putStrLn $ " ├" ++ replicate (maxNameLength + 2) '─'
-                   ++ "┴"
-                   ++ replicate (maxScoreLength + 2) '─'
-                   ++ "╯"
+    putStrLn $ "╰┬" ++ replicate maxNameLength '─'
+                   ++ "─┴─"
+                   ++ replicate maxScoreLength '─'
+                   ++ "─╯"
 
     where maxNameLength  = maximumDef 0 $ map (length . name)         players
           maxScoreLength = maximumDef 0 $ map (length . show . score) players
 
 --------------------------------------------------------------------------------
 printError :: String -> IO ()
-printError string = putStrLn $ " │ Error: " ++ string
+printError string = putStrLn $ " │ ↑ Error: " ++ string
 
 --------------------------------------------------------------------------------
 toEither :: a -> Maybe b -> Either a b
@@ -74,6 +71,17 @@ ask prompt = do
     getLine
 
 --------------------------------------------------------------------------------
+-- append the current scores to a log file; errors gracefully
+-- writeLogFile :: FilePath -> [Player] -> Either String (IO ())
+-- writeLogFile path players = (\exists -> if
+--     | exists -> writeFile path
+--         =<< show . (players:) -- ::
+--         =<< (toEither (path++" is not a valid log file") . readMay) -- :: Either String (IO [Player])
+--         <$> readFile path -- :: IO String
+--     | _ -> Left $ path ++ " does not exist"
+--     ) <$> doesFileExist path
+
+--------------------------------------------------------------------------------
 addPlayer :: String -> [Player] -> [Player]
 addPlayer playerName = sort . (Player 0 playerName :)
 
@@ -82,11 +90,12 @@ rmPlayer :: String -> [Player] -> [Player]
 rmPlayer playerName = sort . filter ((playerName/=) . name)
 
 --------------------------------------------------------------------------------
+-- execute an input of the form '[int] [team name]' on the team list
 addPoints :: Int -> String -> [Player] -> [Player]
 addPoints points playerName players =
     sort $ map (\(Player s n) ->
-        if | n == playerName -> Player (s + points) n
-           | otherwise       -> Player s n
+        if | playerName `isPrefixOf` n -> Player (s + points) n
+           | otherwise -> Player s n
     ) players
 
 --------------------------------------------------------------------------------
@@ -94,6 +103,7 @@ mulPoints :: Int -> [Player] -> [Player]
 mulPoints c = map (\(Player s n) -> Player (c*s) n)
 
 --------------------------------------------------------------------------------
+-- parse an input of the form '[int] [team name]' into a transformation
 parseAddPoints :: String -> Either String ([Player] -> [Player])
 parseAddPoints input = case (length inputWords) of
     1 -> Right $ addPoints 1 player
@@ -104,6 +114,7 @@ parseAddPoints input = case (length inputWords) of
           player = last inputWords
 
 --------------------------------------------------------------------------------
+-- parse a ':' command into a team list transformation; errors gracefully
 parseCommand :: String -> Either String ([Player] -> [Player])
 parseCommand input = (\x -> case x of
         "add" -> toEither "no arguments" $ foldl1May (.) $ map addPlayer args
@@ -119,6 +130,7 @@ parseCommand input = (\x -> case x of
           arg i   = toEither "insufficient arguments" $ atMay args i
 
 --------------------------------------------------------------------------------
+-- turns valid input into a transformation of the team list; errors gracefully
 parse :: String -> Either String ([Player] -> [Player])
 parse input = if
     | null inputWords   -> Left "no input"
@@ -127,6 +139,7 @@ parse input = if
     where inputWords = words input
 
 --------------------------------------------------------------------------------
+-- takes the team list as well as user input and decides how to process it
 process :: [Player] -> String -> IO ()
 process players input
     | elem input [":q", ":quit", ":exit"] = return ()
@@ -139,14 +152,16 @@ process players input
         $ parse input
 
 --------------------------------------------------------------------------------
+-- main loop; carries the team list with it through infinity
 loop :: [Player] -> IO ()
 loop players = do
     printPlayers players
-    process players =<< ask " │"
+    process players =<< ask " │ "
 
 --------------------------------------------------------------------------------
+-- initialize the teams from command-line arguments
 main :: IO ()
 main = do
-    putStrLn " ╭──────────────────────────────────────"
+    putStrLn " ╭─────────────────────────────────────"
     putStrLn " │ enter ':help' for a list of commands"
     loop =<< map (Player 0) <$> getArgs
